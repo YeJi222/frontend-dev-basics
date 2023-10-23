11aad219c49a0e0f91771032b82663a53bc8b7f9
YeJi222 <hhongyizi@gmail.com>
2023-10-23 17:07:34 +0900

    feat: jquery assignment-#finish

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,7 +1,39 @@
+var changeImage = function() {
+	// 랜덤 변수 
+	var index = Math.floor(Math.random() * imageViewer._images.length);
+	var info = imageViewer._images[index].split(":");
+	// console.log(info);
+	
+	$('img')
+		.attr({
+			"src": "/ch07/images/" + info[1],
+			"alt": info[0],
+			"title": info[0]
+		});
+}
+
 var imageViewer = {
-	init: function(){
+	init: $(function(){
+		$("#btn-change").click(function(){
+			changeImage();
+		});
 		
-	},
+		$("#btn-slideshow").click(function(){
+			var slideshow_btn = $('#btn-slideshow').text();
+			if(slideshow_btn == '슬라이드쇼 시작'){
+				$('#btn-slideshow').html('<button id="btn-slideshow">슬라이드쇼 중지</button>');
+			
+				interval = setInterval(function(){
+					changeImage();
+				}, 1500);
+			} else{
+				$('#btn-slideshow').html('<button id="btn-slideshow">슬라이드쇼 시작</button>');
+				
+				clearInterval(interval);
+				$('img').removeAttr('src alt title');
+			}
+		});
+	}),
 	
 	_images: [ 
 		"국화:Chrysanthemum.jpg", 
@@ -14,3 +46,43 @@ var imageViewer = {
 		"툴립:Tulips.jpg" 
 	]
 }
+
+
+/*
+var changeImage = function() {
+	// 랜덤 변수 
+	var index = Math.floor(Math.random() * imageViewer._images.length);
+	var info = imageViewer._images[index].split(":");
+	// console.log(info);
+	
+	$('img')
+		.attr({
+			"src": "/ch07/images/" + info[1],
+			"alt": info[0],
+			"title": info[0]
+		});
+}
+
+$(function(){
+	
+	$("#btn-change").click(function(){
+		changeImage();
+	});
+	
+	$("#btn-slideshow").click(function(){
+		var slideshow_btn = $('#btn-slideshow').text();
+		if(slideshow_btn == '슬라이드쇼 시작'){
+			$('#btn-slideshow').html('<button id="btn-slideshow">슬라이드쇼 중지</button>');
+		
+			interval = setInterval(function(){
+				changeImage();
+			}, 1500);
+		} else{
+			$('#btn-slideshow').html('<button id="btn-slideshow">슬라이드쇼 시작</button>');
+			
+			clearInterval(interval);
+			$('img').removeAttr('src alt title');
+		}
+	});
+});
+*/
